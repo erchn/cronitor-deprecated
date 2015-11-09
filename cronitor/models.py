f9b2626f2e063737e8822beb9402bad9d7e94bd6
Ethan Erchinger <ethan@erchinger.org>
2015-11-09 18:49:22 +0100

    first basic models, probably overkill

---
+++ cronitor/models.py
@@ -0,0 +1,78 @@
+"""This class defines a cronitor Monitor object.
+
+It contains a number of methods that can be called:
+
+  - create # create the monitor at the Cronitor site
+  - delete # delete the monitor ...
+  - update # update the monitor ...
+  - run # mark the monitor as just started
+  - complete # mark successful finish of monitor
+  - fail # mark failure at Cronitor for this monitor
+"""
+
+import json
+
+class Rule(object):
+    def __init__(self, rtype, rdur, runit, rfol=None):
+        '''Create Notification object.
+
+        One Rule object at a time, 3 required, one optional arg:
+          - notification type (string)
+          - notification duration (integer)
+          - time unit (string)
+          - followup hours (integer)
+
+        More details are available here: 
+          https://cronitor.io/help/monitor-api
+        '''
+        self.type = rtype
+        self.dur = rdur
+        self.unit = runit
+        self.followup = rfol
+
+        fields = ['rule_type', 'duration', 'time_unit']
+        if self.followup is not None:
+            fields.append('followup')
+
+        self.output = dict(zip(fields, [self.type, self.dur, self.unit, self.followup]))
+
+    def __str__(self):
+        return json.dumps(self.output)
+
+class Notification(object):
+    def __init__(self, ntype, ndests):
+        '''Create Notification object.
+
+        One Notification type at a time, with an array of values
+        Types are available here: 
+          https://cronitor.io/help/monitor-api
+        '''
+        self.type = ntype
+        self.dests = ndests
+
+    def __str__(self):
+        return json.dumps({self.type: self.dests})
+
+class Monitor(object):
+    def __init__(self, name, **kwargs):
+        """Create monitor object, name arg is required.
+
+        Pass all other details as keyword args.
+        On init, only one notification or rule can be created,
+        use add_rule or add_notif to add additional.
+        """
+        self.name = name
+        self.notifications = []
+        self.rules = []
+        if kwargs.get("notification", None) is not None:
+            self.notifications = [Notification(kwargs.get("notification"))]
+        if kwargs.get("rule", None) is not None:
+            self.rules = [Rule(kwargs.get("rule"))]
+        self.note = kwargs.get("note", "")
+
+
+    def add_rule(self, rtype, rdur, runit, rfol):
+        self.rules.append(Rule(rtype, rdur, runit, rfol))
+
+    def add_notif(self, ntype, ndests):
+        self.notifications.append(Notification(ntype, ndests))
